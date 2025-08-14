4ee4cb15a9e13c98fa9134f1f564d2ae5a09e0c7
ma1ono <whitebeatstore@gmail.com>
2025-08-14 18:32:02 +0500

    array

--- массивы/practicle/js/main.js
+++ массивы/practicle/js/main.js
@@ -1,64 +1,111 @@
-const inputElement = document.getElementById('title')
-const createBtn = document.getElementById('create')
-const listElement = document.getElementById('list')
+const inputElement = document.getElementById('title');
+const createBtn = document.getElementById('create');
+const listElement = document.getElementById('list');
 
+// const notes = [
+//     'записаться к врачу', 
+//     'hello world'
+// ]
 
-createBtn.addEventListener('click', function () {
-    addTask()
-})
+// function render() {
+//     for (let i = 0; i < notes.length; i++) {
+//         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+//     }
 
-function addTask() {
-    if (inputElement.value === '') {
-        alert('Введите задачу')
-    } else {
-        let li = document.createElement('li')
-        li.innerHTML = inputElement.value
-        listElement.appendChild(li)
-        let span = document.createElement('span')
-        span.inner = '\u00d7'
-        li.appendChild(span)
-    }
+//     // for (let note of notes) {
+//     //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
+//     // }
+// }
 
-    inputElement.value = '';
-    saveData()
-}
+// render()
 
-listElement.addEventListener('click', function (e) {
+// createBtn.onclick = function () {
+//     if (inputElement.value.length = 0) {
+//         return
+//     }
+//     listElement.insertAdjacentHTML(
+//         'beforeend',
+//         getNoteTemplate(inputElement.value)
+//     )
+//     inputElement.value = ''
+// }
 
-    if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('checked') 
-        saveData
-    } else if (e.target.tagName === 'SPAN') {
-        e.target.parentElement.remove()
-        saveData
-    }
-})
-
-function saveData () {
-    localStorage.setItem('data', listElement.innerHTML)
-}
-function showData () {
-    listElement.innerHTML = localStorage.getItem('data')
-}
-
-showData();
+// function getNoteTemplate(title) {
+//     return`<li
+//           class="list-group-item d-flex justify-content-between align-items-center"
+//         >
+//           <span>${title}</span>
+//           <span>
+//             <span class="btn btn-small btn-success">&check;</span>
+//             <span class="btn btn-small btn-danger">&times;</span>
+//           </span>
+//         </li>`
+// }
 
 const notes = [
     {
-    title: 'записаться к врачу',
-    completed: false,
+        title: 'записать блок про массивы',
+        completed: false,
     },
     {
-    title: 'записаться к врачу',
-    completed: false,
+        title: 'записать блок про массивы',
+        completed: true,
     },
-    {
-    title: 'записаться к врачу',
-    completed: false,
-    }
 ]
 
 function render() {
-
+    listElement.innerHTML = ""
+    if (notes.length === 0) {
+        listElement.innerHTML = '<p>Нет элементов</p>'
+    }
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+    }
 }
 render()
+
+createBtn.onclick = function () {
+    if (inputElement.value.length = 0) {
+        return
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+    notes.push(newNote)
+    render()
+    // listElement.insertAdjacentHTML(
+    //     'beforeend',
+    //     getNoteTemplate(newNote)
+    // )
+    inputElement.value = ''
+}
+
+listElement.onclick = function (event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if (type === 'toggle') {
+            notes[index].completed = !notes[index].completed
+            console.log('toggle', index)
+        } else if (type === 'remove') {
+            notes.splice(index, 1)
+            console.log('remove', index)
+        }
+
+        render()
+    }
+}
+
+function getNoteTemplate(note, index) {
+    return`<li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+          <span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span> 
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
+          </span>
+        </li>`
+}
--- массивы/theory/theory.js
+++ массивы/theory/theory.js
@@ -15,9 +15,116 @@ const array = [1, 2, 3]
 // array[0] = 'Privet' console.log(array) - заменит нулевой массив на слово
 // array[array.length] = 'becon' console.log(array)
 
+
+//  Методы массивов
+const names = ['Vlad', 'Igor', 'Elena', 'Csenia']
+
+// names.push('Alex', 'Andrey') - правильный варинт, чтобы положить объект в массив, но в конец
+// names.unshift('Alex')
+/** Методы пуш и аншифт отличаются по скорости работы, метод пуш работает быстрей, потому что ему нужно меньше времени на вычисление*/
+// const firstName = names.shift('') - удаляет первый элемент из массива
+// const name = names.pop() - удаляет последний элемент, вытаскивает из массива, добавляет его в отдельную переменную
+// console.log(names)
+// console.log(names. reverse()) - мутирует изначальный массив
+// console.log(names.toReversed()) -  возвращает новый массив и при этом не меняет оригинал
+// console.log(letters.sort()) - сортирует положение в алф порядке, мутирует массив
+// return b.charCodeAt(0) - a.charCodeAt(0)
+// return a.charCodeAt(0) - b.charCodeAt(0) 
+// console.log(letters.toSorted()) - новый метод
+// .splice - принимает в себя индекс того элемента, который хотим видить или с того, с которого хотим начать
+// .toSpliced - новый метод, не мутирующий массив
+
+// const greateWoman = 'Elena'
+// const index = names.indexOf(greateWoman)
+// console.log(index)
+// const newNames = names.with(index, 'Elena Velikaia')
+// // console.log(names)
+// // console.log(newNames)
+// // names[index] = 'Elena Velikaia'
+// // console.log(names[index])
+
+// names.map(function(name) {
+//     const newName = name + '!'
+// })
+// console.log(names)
+
+// const capitalNames = names.map(function(name, index) {
+//     // const newName = name + '!'
+//     // return newName
+//     // return name.toUpperCase()
+//     if (index === 1) {
+//         return "ELena Velikaia"
+//     }
+//     return name
+// })
+// console.log(capitalNames)
+
+// console.log(names.includes('Igor'))
+// console.log(names.indexOf('Igor') !== -1)
+
+const people = [
+    {name: 'Vlad', budget: 4200},
+    {name: 'Elena', budget: 1200},
+    {name: 'Igor', budget: 3200},
+    {name: 'Csenia', budget: 85200}
+]
+
+// console.log(people.indexOf({name: 'Vlad', budget: 4200}))
+// let findedPerson 
+// for (let person of people) {
+//     if (person.budget === 4200) {
+//         findedPerson = person
+//     }
+// }
+// console.log(findedPerson)
+
+
+// const findedPerson = people.find(function(person) {
+//     return person.budget === 4200;
+//     if (person.budget === 4200) {
+//         return true
+//     }
+// })
+
+// const finded = people.find((p) => p.budget === 4200)
+
+// const finded = people.findIndex(function(person) {
+//     return person.budget === 4300;
+    
+// })
+// console.log(finded)
+
+// const filter = people.filter(function(p) {
+//     return p.budget > 4000
+// })
+// console.log(filter)
+
+// let sumBud = 0
+// const filter = people.filter(function(p) {
+//     return p.budget > 4000
+// })
+// console.log(filter)
+// filter.forEach(function(p) {
+//     sumBud = sumBud + p.budget
+// })
+// console.log(sumBud)
+
+// const sumBud = people
+//     .filter((p) => p.budget > 5000)
+//     .map((p) => p.budget)
+//     .reduce((acc, p) => acc + p, 0)
+// console.log(sumBud)
+
+// const string = 'hello'
+// const reversed = string.split('').toReversed().join('/').split((c) => c !==='!')
+// console.log(reversed)
+
+
+
+
 /** 
  * Объекты
- */
+
 
 const person = {
     firstName: 'Alex',
@@ -41,3 +148,5 @@ person.hasGirlFriend = true
 console.log(person[key])
 
 person.getFullName()
+ */
+
